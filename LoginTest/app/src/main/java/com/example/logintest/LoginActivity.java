700a6b12e8bcd2b57670292d4c5096a313b214cb
ostandddd <103240800+ostandddd@users.noreply.github.com>
2022-04-21 12:32:23 +0800

    Add files via upload

---
+++ LoginTest/app/src/main/java/com/example/logintest/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.logintest;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends BaseActivity{
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private EditText userEdit;
+    private EditText passwordEdit;
+    private Button login;
+    private CheckBox rememberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        userEdit = (EditText) findViewById(R.id.user);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+        login = (Button) findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember_password",false);
+        if (isRemember){
+            String user = pref.getString("user","");
+            String password = pref.getString("password","");
+            userEdit.setText(user);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = userEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                if (user.equals("admin")&&password.equals("123456")){
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("user",user);
+                        editor.putString("password",password);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.commit();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this,"用户名或密码错误",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
